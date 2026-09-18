76d7c23ff3053040d0dcde9f0dfebe3c6f14d7e3
Ghulam Ali <gal775322@gmail.com>
2026-09-18 15:36:00 +0500

    Add files via upload

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using LeadsSMS.Models;
+
+namespace LeadsSMS.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<EmployeeAttendance> EmployeeAttendances { get; set; }
+        public DbSet<EmployeeSalary> EmployeeSalaries { get; set; }
+        public DbSet<StudentFee> StudentFees { get; set; }
+        public DbSet<Promotion> Promotions { get; set; }
+    }
+}
